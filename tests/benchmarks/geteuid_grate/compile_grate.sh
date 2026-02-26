47e352d93459c223663dac0c4017b53192ecab00
Sanchit Sahay <sanchitsahay.tech@gmail.com>
2026-02-26 02:14:21 -0500

    Remove local grate files

--- tests/benchmarks/geteuid_grate/compile_grate.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-cd "$(dirname "$0")"
-lind_compile --compile-grate geteuid_grate.c
--- tests/benchmarks/imfs_grate/compile_grate.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-cd "$(dirname "$0")"
-lind_compile --compile-grate src/imfs_grate.c src/imfs.c
